86bff740a27fa73271d2ba37517e4865a121b758
Phvr06 <ribeirophvr@gmail.com>
2023-05-04 09:11:16 -0300

    import, init e tela

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+WIDTH = 800 # pixels
+HEIGHT = 400 # pixel
+
+# Inicia o pygame
+pygame.init()
+
+# Gera a tela na qual o jogo será jogado. Recebe como argumento um tuple que consiste da largura e da altura
+tela = pygame.display.set_mode((WIDTH, HEIGHT))
+
