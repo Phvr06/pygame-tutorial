a96725db00b4d74c4aca1ad2eaa7c365d17d6de4
Phvr06 <ribeirophvr@gmail.com>
2023-05-04 09:18:58 -0300

    loop do jogo, update da tela e fechar o jogo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+# Import para poder parar a execução de todo o código caso o jogador feche o jogo
+from sys import exit
 
 WIDTH = 800 # pixels
 HEIGHT = 400 # pixel
@@ -9,3 +11,12 @@ pygame.init()
 # Gera a tela na qual o jogo será jogado. Recebe como argumento um tuple que consiste da largura e da altura
 tela = pygame.display.set_mode((WIDTH, HEIGHT))
 
+while True:
+    # Loop para pegar os possíveis inputs do jogador
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    # Atualiza a tela que foi gerada anteriormente com novas informações que tenham ocorrido durante o loop
+    pygame.display.update()
