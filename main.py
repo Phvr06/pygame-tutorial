e9126de55733f6f9552d581761224ecf731fba03
Phvr06 <ribeirophvr@gmail.com>
2023-05-04 14:26:43 -0300

    criando superfície teste, posicionando, mudando cor e mostrando na tela + comentário triste ao fechar o jogo

--- main.py
+++ main.py
@@ -17,12 +17,23 @@ pygame.display.set_caption('JOGO DO DINOSSAURO FAKE')
 # Clock para definir o framerate do jogo
 clock = pygame.time.Clock()
 
+# Superfície para testar
+# Superfícies são os elementos visuais que serão mostrados na tela
+superficie_teste = pygame.Surface((100, 200))
+
+# Adiciona a cor vermelha à essa superfície teste 
+superficie_teste.fill('Red')
+
 while True:
     # Loop para pegar os possíveis inputs do jogador
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
+            print("\nNão me deixe sozinho :(\n")
             pygame.quit()
             exit()
+
+    # Mostra na tela a superfície teste criada
+    tela.blit(superficie_teste, (200, 100))
 
     # Atualiza a tela que foi gerada anteriormente com novas informações que tenham ocorrido durante o loop
     pygame.display.update()
