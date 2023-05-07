128e19bf1a310735ec971e2b3243861defd926dc
Phvr06 <ribeirophvr@gmail.com>
2023-05-07 18:26:27 -0300

    placar e variável de estado

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pygame
 # Import para poder parar a execução de todo o código caso o jogador feche o jogo
 from sys import exit
 
+def placar():
+    tempo = pygame.time.get_ticks()
+    superficie_tempo = fonte.render(f'Pontos: {int((tempo-tempo_inicial)/1000)}',False,(64,64,64))
+    retangulo_tempo = superficie_tempo.get_rect(center = (400,50))
+    tela.blit(superficie_tempo,retangulo_tempo)
+
 WIDTH = 800 # pixels
 HEIGHT = 400 # pixel
 
@@ -20,16 +26,20 @@ clock = pygame.time.Clock()
 # Define uma fonte para ser utilizada ao escrever textos que vão ser exibidos no jogo como uma superfície
 fonte = pygame.font.Font('font/Pixeltype.ttf', 50)
 
+# Variável de estado do jogo
+jogando = True
+tempo_inicial = 0
+
 # Superfícies são os elementos visuais que serão mostrados na tela
 # Retângulos são elementos utilizados para posicionar melhor superfícies
 # Superfície de céu para o fundo
 superficie_sky = pygame.image.load('graphics/Sky.png').convert()
 # Superfície de chão para o fundo
 superficie_ground = pygame.image.load('graphics/ground.png').convert()
-# Superfície do texto
-superficie_texto = fonte.render("Alien :)", False, (64,64,64))
-# Retângulo do texto
-retangulo_texto = superficie_texto.get_rect(center = (400, 50))
+# # Superfície do texto
+# superficie_texto = fonte.render("Alien :)", False, (64,64,64))
+# # Retângulo do texto
+# retangulo_texto = superficie_texto.get_rect(center = (400, 50))
 # Superfície do inimigo
 superficie_lesma = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 # Retângulo lesma
@@ -47,34 +57,50 @@ while True:
             print("\nNão me deixe sozinho :(\n")
             pygame.quit()
             exit()
-        # Colisão com o mouse
-        if evento.type == pygame.MOUSEBUTTONDOWN and retangulo_player.collidepoint(evento.pos) and retangulo_player.bottom >= 300:
-            gravidade = -20
-        # Receber input do teclado
-        if evento.type == pygame.KEYDOWN and evento.key == pygame.K_SPACE and retangulo_player.bottom >= 300:
-            gravidade = -20
+        if jogando:
+            # Colisão com o mouse
+            if evento.type == pygame.MOUSEBUTTONDOWN and retangulo_player.collidepoint(evento.pos) and retangulo_player.bottom >= 300:
+                gravidade = -20
+            # Receber input do teclado
+            if evento.type == pygame.KEYDOWN and evento.key == pygame.K_SPACE and retangulo_player.bottom >= 300:
+                gravidade = -20
+        else:
+            if evento.type == pygame.KEYDOWN and evento.key == pygame.K_SPACE:
+                jogando = True
+                retangulo_lesma.left = 800
+                tempo_inicial = pygame.time.get_ticks()
 
-    # Mostra na tela as superfícies do céu e do chão e do texto
-    tela.blit(superficie_sky, (0, 0))
-    tela.blit(superficie_ground, (0, 300))
-    pygame.draw.rect(tela, "#c0e8ec", retangulo_texto)
-    pygame.draw.rect(tela, "#c0e8ec", retangulo_texto, 10)
-    tela.blit(superficie_texto, retangulo_texto)
-    # If/else para voltar a lesma para a tela quando ela sair dela
-    if retangulo_lesma.right > 0:
-        # Mexe a lesma 4 pixel para a esquerda
-        retangulo_lesma.x -= 4
+
+    if jogando:
+        # Mostra na tela as superfícies do céu e do chão e do texto
+        tela.blit(superficie_sky, (0, 0))
+        tela.blit(superficie_ground, (0, 300))
+        # pygame.draw.rect(tela, "#c0e8ec", retangulo_texto)
+        # pygame.draw.rect(tela, "#c0e8ec", retangulo_texto, 10)
+        # tela.blit(superficie_texto, retangulo_texto)
+        placar()
+        # If/else para voltar a lesma para a tela quando ela sair dela
+        if retangulo_lesma.right > 0:
+            # Mexe a lesma 4 pixel para a esquerda
+            retangulo_lesma.x -= 4
+        else:
+            retangulo_lesma.left = 800
+        # Mostra na tela a superfície da lesma
+        tela.blit(superficie_lesma, retangulo_lesma)
+
+        gravidade += 1
+        retangulo_player.y += gravidade
+        if retangulo_player.bottom >= 300:
+            retangulo_player.bottom = 300
+        # Mostra na tela a superfície do jogador
+        tela.blit(superficie_player, retangulo_player)
+
+        # Colisão da lesma com o player
+        if retangulo_lesma.colliderect(retangulo_player):
+            jogando = False
     else:
-        retangulo_lesma.left = 800
-    # Mostra na tela a superfície da lesma
-    tela.blit(superficie_lesma, retangulo_lesma)
+        tela.fill('Yellow')
 
-    gravidade += 1
-    retangulo_player.y += gravidade
-    if retangulo_player.bottom >= 300:
-        retangulo_player.bottom = 300
-    # Mostra na tela a superfície do jogador
-    tela.blit(superficie_player, retangulo_player)
 
     # Atualiza a tela que foi gerada anteriormente com novas informações que tenham ocorrido durante o loop
     pygame.display.update()
